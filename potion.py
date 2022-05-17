692eaadd686e1994d02b30c9ed746483aff2bee1
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2022-05-17 19:56:11 +1000

    returning all the primes till k

--- potion.py
+++ potion.py
@@ -1,7 +1,10 @@
 class Potion:
     
     def __init__(self, potion_type: str, name: str, buy_price: float, quantity: float) -> None:
-        raise NotImplementedError()
+        self.potion_type = potion_type
+        self.name = name
+        self.buy_price = buy_price
+        self.quantity = quantity
 
     @classmethod
     def create_empty(cls, potion_type: str, name: str, buy_price: float) -> 'Potion':
