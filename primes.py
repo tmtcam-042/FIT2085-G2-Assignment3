e17cf156adaf8dbea9fb9a624602b027028cdac5
Kisara Batugedara <kbat0008@student.monash.edu>
2022-05-24 16:33:07 +1000

    Fixed a small error in Prime Number Generation in primes.py.

--- primes.py
+++ primes.py
@@ -7,15 +7,35 @@ def largest_prime(k: int) -> int:
 
     return all i such that A[i] is true.
     """
-    A = [True for _ in range(k-1)]
-    A[0] = False
-    A[1] = False
-    for i in range(2, int(k**0.5) + 1):
-        if A[i]:
-            for j in range(i**2, k-1, i):
-                A[j] = False
+    # A = [True for _ in range(k-1)]
+    # A[0] = False
+    # A[1] = False
+    # for i in range(2, int(k**0.5) + 1):
+    # if A[i]:
+    # for j in range(i**2, k-1, i):
+    # A[j] = False
 
-    return max([i for i, b in enumerate(A) if b])
+    # return max([i for i, b in enumerate(A) if b])
+
+    primes_list = [True for i in range(k + 1)]
+    p = 2
+
+    while p * p <= k:
+
+        # If prime[p] is not changed, then it is a prime
+        if primes_list[p]:
+
+            # Update all multiples of p
+            for i in range(p ** 2, k + 1, p):
+                primes_list[i] = False
+
+        p += 1
+
+    primes_list[0] = False
+    primes_list[1] = False
+    # Print all prime numbers
+    return max([i for i, b in enumerate(primes_list) if b])
+
 
 if __name__ == "__main__":
-    print(largest_prime(100))
+    print(largest_prime(159))
