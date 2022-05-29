2a5af56ec429856f7654365b3a299b28310c959d
tmtcam-042 <90966515+tmtcam-042@users.noreply.github.com>
2022-05-29 17:52:33 +1000

    Added docstring for primes.py

--- primes.py
+++ primes.py
@@ -1,17 +1,20 @@
 def largest_prime(k: int) -> int:
     """
-    for i = 2, 3, 4, ..., not exceeding √n do
-    if A[i] is true
-        for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n do
-            set A[j] := false
-
-    return all i such that A[i] is true.
+    A function designed to return the largest prime number less than k.
+    Implemented using the Sieve of Eratosthenes algorithm.
+    :pre: k must be an integer >= 3
+    :post: the integer returned is strictly less than k
+    :complexity: best and worst O(N log(log N)) where N is the value of k
+    :return: the largest prime number strictly less than k
     """
+
+    if k < 3:
+        raise ValueError("Input must be an integer >= 3")
 
     primes_list = [True for i in range(k)]
     p = 2
 
-    while p * p <= k:
+    while p ** 2 <= k:
 
         # If prime[p] is not changed, then it is a prime
         if primes_list[p]:
