ba5881255b8ec359f5e2f0bbddb8423238ba617d
tmtcam-042 <90966515+tmtcam-042@users.noreply.github.com>
2022-05-29 20:51:12 +1000

    Updated game.py to most recent

--- game.py
+++ game.py
@@ -1,12 +1,8 @@
-from typing import List, Tuple
-
 from linked_stack import LinkedStack
 from random_gen import RandomGen
 from hash_table import LinearProbePotionTable
 from potion import Potion
 from avl import AVLTree
-from array_list import ArrayList
-from stack_adt import Stack
 
 
 class Game:
@@ -29,7 +25,7 @@ class Game:
             self.potion_table[name] = Potion.create_empty(_type, name, price)
             print(self.potion_table)
 
-    def add_potions_to_inventory(self, potion_name_amount_pairs: List[Tuple[str, float]]) -> None:
+    def add_potions_to_inventory(self, potion_name_amount_pairs: list[tuple[str, float]]) -> None:
         """
         Binary tree!
         :param potion_name_amount_pairs:
@@ -38,8 +34,8 @@ class Game:
         """
         for potion in potion_name_amount_pairs:
             name, amount = potion
+            self.potion_table[name].quantity = amount
             potion_object = self.potion_table[name]
-            potion_object.quantity = amount
             self.inventory[potion_object.buy_price] = (potion_object, amount)
         self.inventory.print_tree()
 
@@ -52,14 +48,13 @@ class Game:
         saved_inventory = self.inventory
         for i in range(num_vendors):
             p = self.rand.randint(len(self.inventory))
-            pth_index = len(self.inventory) - p
 
-            for j, key in reversed(list(enumerate(self.inventory))):
+            for j, key in enumerate(self.inventory):
                 # p starts from 1 - k, hence we add 1 to j
-                if pth_index == j:
+                if p == j + 1:
                     node = self.inventory.get_tree_node_by_key(key).item
                     name, amount = node[0].name, node[1]
-                    self.potion_table[name].quantity = amount
+                    # self.potion_table[name].quantity = amount
                     vendor_potion_list.append((name, amount))
                     del self.inventory[key]
                     break
@@ -68,26 +63,43 @@ class Game:
         self.inventory.print_tree()
         return vendor_potion_list
 
-    def solve_game(self, potion_valuations: List[Tuple[str, float]], starting_money: List[int]) -> List[float]:
+    def solve_game(self, potion_valuations: list[tuple[str, float]], starting_money: list[int]) -> list[float]:
         """
+
         potion_valuations: is a list of potions that each vendor is selling, paired with its valuation
                            by the adventurers
         starting_money: is a list containing, for each attempt, the starting allowance the player has.
 
         required complexity: 𝐎(𝑁 × log(𝑁) + 𝑀 × 𝑁)
         """
+
         day_profits = []
         ratio_tree = AVLTree()
-        for i in range(len(potion_valuations)):  # O(N)
+
+        """
+        This for loop goes through each potion_valuation and creates the binary tree.
+        :pre: this list of potion_valuation must contain 1 or more elements
+        :raises ValueError: if the list is empty
+        :complexity best: O(N) x O(log(N))
+        :complexity worst: O(N^2) 
+        :average complexity: O(N) x O(log(N))
+        """
+        # TODO: CHECK THIS IS RIGHT AND GET RID OF PRINT STATEMENTS
+        for i in range(len(potion_valuations)): # Loop time complexity: O(N) where N is the length of potion_valuations
+
+            if len(potion_valuations) == 0:
+                raise ValueError(f"List has length: {len(potion_valuations)}")
+
             name, valuation = potion_valuations[i]
             vendor_buy_price = self.potion_table[name].buy_price
             profit_margin = valuation - vendor_buy_price
             ratio = profit_margin / vendor_buy_price
             quantity = self.potion_table[name].quantity
+
+            print(f"Ratios: {ratio}")
             print(f"Quantity: {quantity}")
 
-            # assuming normal potions
-            if ratio not in ratio_tree:
+            if ratio not in ratio_tree:     # Binary Tree Insertion Time complexity: O(log(N))
                 ratio_tree[ratio] = (False, (name, vendor_buy_price, valuation, profit_margin, ratio, quantity))
             else:
                 tree_stack = LinkedStack()
@@ -97,45 +109,66 @@ class Game:
                 tree_stack.push((name, vendor_buy_price, valuation, profit_margin, ratio, quantity))
                 ratio_tree[ratio] = (True, tree_stack)
 
-        for money in starting_money:  # O(M)
+        """
+            Iterates through the money values for each day and calculates the money remaining 
+            at the end of each iteration(day)
+            
+            :pre: the tree must exist and its root must not be None
+            :raises Exception: if the tree does not exist
+            :pre: there must be integer values in starting_money list
+            :raises Exception: if the list has not been defined
+            :complexity best: O(M) x O(log(N))
+            :complexity worst: O(M) x O(N)
+            :average complexity: O(M) x O(N)
+        """
+        for money in starting_money:    # Loop Time complexity: O(M)
+
+            if ratio_tree.root is None:
+                raise TypeError("Ratio tree does not exist")
+            elif len(starting_money) == 0:
+                raise ValueError("List has length 0")
+
             checked = []
-            dup_checker = []
+            temporary_stack = LinkedStack()
             profit_for_day = 0
+            print(f"\nStaring Day Money: {money}")
+
             while money > 0:
-                max_ratio = 0
-                for ratio in ratio_tree:  # O(N)
-                    if ratio > max_ratio and not (ratio in checked):
+                max_ratio = ratio_tree.get_minimal(ratio_tree.root).key
+                for ratio in ratio_tree:  # Loop Time complexity: O(N)
+                    if ratio >= max_ratio and not (ratio in checked):
                         max_ratio = ratio
 
-                # check is the current ratio is in the duplicate list
                 best_ratio_item = ratio_tree[max_ratio]
-                if best_ratio_item[0]:
-                    stack_item = best_ratio_item[1]
-                    check_item = stack_item.peek()
-                    if check_item[0] not in dup_checker:
-                        item = stack_item.peek()
-                        dup_checker.append(item[0])
-                    else:
-                        temp_item = stack_item.pop()
-                        item = stack_item.peek()
-                        stack_item.push(temp_item)
+                original_stack = best_ratio_item[1]
+
+                if best_ratio_item[0]:  # checks is the stack is True (duplicate)
+                    item = original_stack.pop()  # pops it off
+                    temporary_stack.push(item)  # pushes it into the temporary stack
+
+                    if original_stack.is_empty():  # if the stack is empty (no more duplicates)
+                        checked.append(max_ratio)  # add the node to visited nodes
+                        del ratio_tree[max_ratio]  # delete the node at that key
+                        ratio_tree[max_ratio] = (True, temporary_stack) # reset it by putting the temp stack in place of the whole stack
                 else:
-                    checked.append(max_ratio)
-                    item = best_ratio_item[1]
-                print(item)
-                name, vendor_buy_price, valuation, profit_margin, ratio, quantity = item
-                # when we can buy all of the potion. -> Potion finishes
-                if money >= quantity * vendor_buy_price:
-                    profit_for_day += quantity * valuation  # Money earned from sale of potion
-                    print(quantity)
-                    money -= quantity * vendor_buy_price  # Available money is reduced
-                else:
-                    # we spend all our money buying the potions
-                    # (which is available in sufficient quantity) -> Money Finishes
+                    checked.append(max_ratio)  # if no duplicate, add key to checked list
+                    item = best_ratio_item[1]  # item is the second element bc there is no stack
+
+                print(f"Potion bought: {item}")
+                name, vendor_buy_price, valuation, profit_margin, ratio, quantity = item  # split item into its parts
+
+                if money >= quantity * vendor_buy_price:  # if we can buy the whole inventory and still have money left
+                    profit_for_day += quantity * valuation  # whole inventory * adventurer buy price
+                    print(f"Bought the whole stock: {quantity}L for ${vendor_buy_price} each\n")
+                    money -= quantity * vendor_buy_price  # subtract this from the money
+                    print(f"Money left: {money}")
+                else:  # bought the whole inventory
+                    if best_ratio_item[0]:  # if it was a part of a duplicate and it was only the first one used, put it back
+                        original_stack.push(item)
                     new_quantity = money / vendor_buy_price  # quantity of potion purchased (L)
-                    print(new_quantity)
+                    print(f"Went broke buying: {new_quantity}L for ${vendor_buy_price} each\n")
                     profit_for_day += new_quantity * valuation  # money earned from sale of potion
-                    money = 0
+                    money = 0  # set money to 0 since we broke
 
             day_profits.append(profit_for_day)
 
