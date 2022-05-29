1d178e84558d567f732796b59ab8281c0086ab50
tmtcam-042 <90966515+tmtcam-042@users.noreply.github.com>
2022-05-29 18:14:23 +1000

    Added or modified docstrings

--- potion.py
+++ potion.py
@@ -14,16 +14,16 @@ class Potion:
 
     @classmethod
     def create_empty(cls, potion_type: str, name: str, buy_price: float) -> 'Potion':
-        """ Creates a potion with corresponding potion type,name and buy price and initialises the quantity of it to
-        0
+        """ Creates a potion with corresponding potion type,name and buy price and
+        initialises the quantity of it to 0
         """
         return Potion(potion_type, name, buy_price, 0)
 
     @classmethod
     def good_hash(cls, potion_name: str, tablesize: int) -> int:
         """
-        Method that hashes a position for a given string and tablesize. This uses a universal hash method to prevent
-        collisions and conflicts
+        Method that hashes a position for a given string and tablesize. This uses
+        a universal hash method to prevent collisions and conflicts
         """
         value = 0
         a = largest_prime(52459)
@@ -36,8 +36,8 @@ class Potion:
     @classmethod
     def bad_hash(cls, potion_name: str, tablesize: int) -> int:
         """
-        Method that hashes a position for a given string and tablesize. This uses a bad hash method to hash a key where
-        lots of collisions and conflicts can occur.
+        Method that hashes a position for a given string and tablesize. This uses
+        a bad hash method to hash a key where lots of collisions and conflicts can occur.
         """
         return ord(potion_name[0]) % tablesize
 
--- primes.py
+++ primes.py
@@ -4,32 +4,37 @@ def largest_prime(k: int) -> int:
     Implemented using the Sieve of Eratosthenes algorithm.
     :pre: k must be an integer >= 3
     :post: the integer returned is strictly less than k
+    :raises ValueError: if k < 3
+    :raises TypeError: if k is not integer
     :complexity: best and worst O(N log(log N)) where N is the value of k
     :return: the largest prime number strictly less than k
     """
 
-    if k < 3:
-        raise ValueError("Input must be an integer >= 3")
+    try:
+        if k < 3:
+            raise ValueError("Input must be >= 3")
+        primes_list = [True for i in range(k)]
+        p = 2
 
-    primes_list = [True for i in range(k)]
-    p = 2
+        while p ** 2 <= k:
 
-    while p ** 2 <= k:
+            # If prime[p] is not changed, then it is a prime
+            if primes_list[p]:
 
-        # If prime[p] is not changed, then it is a prime
-        if primes_list[p]:
+                # Update all multiples of p
+                for i in range(p ** 2, k, p):
+                    primes_list[i] = False
 
-            # Update all multiples of p
-            for i in range(p ** 2, k, p):
-                primes_list[i] = False
+            p += 1
 
-        p += 1
+        primes_list[0] = False
+        primes_list[1] = False
 
-    primes_list[0] = False
-    primes_list[1] = False
+        # Print all prime numbers
+        return max([i for i, b in enumerate(primes_list) if b])
 
-    # Print all prime numbers
-    return max([i for i, b in enumerate(primes_list) if b])
+    except (ValueError, TypeError) as e:
+        print(f"Error {type(e)}: {e}")
 
 
 if __name__ == "__main__":
